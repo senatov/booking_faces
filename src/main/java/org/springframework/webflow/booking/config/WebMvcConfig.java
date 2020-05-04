9e4493b7875b63145f0e631aec3cee0c5a22c8d2
Iakov Senatov <javaEntwickler@gmail.com>
2020-05-04 23:42:30 +0200

    OK/Tested: fixed context in spring-boot

--- src/main/java/org/springframework/webflow/booking/config/WebMvcConfig.java
+++ src/main/java/org/springframework/webflow/booking/config/WebMvcConfig.java
@@ -26,68 +26,70 @@ import static java.util.concurrent.TimeUnit.HOURS;
 @Slf4j
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    public static final CacheControl CACHE_PUBLIC = CacheControl.maxAge(2, HOURS).cachePublic();
-    @Autowired
-    private WebFlowConfig webFlowConfig;
+	public static final CacheControl CACHE_PUBLIC = CacheControl
+			.maxAge(2, HOURS)
+			.cachePublic();
+	@Autowired private WebFlowConfig webFlowConfig;
 
 
+	@Bean
+	public FlowHandlerMapping flowHandlerMapping() {
 
-    @Bean
-    public FlowHandlerMapping flowHandlerMapping() {
-
-        FlowHandlerMapping mapping = new FlowHandlerMapping();
-        mapping.setOrder(1);
-        mapping.setFlowRegistry(webFlowConfig.flowRegistry());
-        /* If no flow matches, map the path to a view, e.g. "/intro" maps to a view named "intro" */
-        mapping.setDefaultHandler(new UrlFilenameViewController());
-        return mapping;
-    }
-
+		FlowHandlerMapping mapping = new FlowHandlerMapping();
+		mapping.setOrder(1);
+		mapping.setFlowRegistry(webFlowConfig.flowRegistry());
+		/* If no flow matches, map the path to a view, e.g. "/intro" maps to a view named "intro" */
+		mapping.setDefaultHandler(new UrlFilenameViewController());
+		return mapping;
+	}
 
 
-    @Bean
-    public FlowHandlerAdapter flowHandlerAdapter() {
+	@Bean
+	public FlowHandlerAdapter flowHandlerAdapter() {
 
-        JsfFlowHandlerAdapter adapter = new JsfFlowHandlerAdapter();
-        adapter.setFlowExecutor(webFlowConfig.flowExecutor());
-        return adapter;
-    }
-
-
-
-    @Bean
-    public UrlBasedViewResolver faceletsViewResolver() {
-
-        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
-        resolver.setViewClass(JsfView.class);
-        resolver.setPrefix("/WEB-INF/");
-        resolver.setSuffix(".xhtml");
-        return resolver;
-    }
+		JsfFlowHandlerAdapter adapter = new JsfFlowHandlerAdapter();
+		adapter.setFlowExecutor(webFlowConfig.flowExecutor());
+		return adapter;
+	}
 
 
+	@Bean
+	public UrlBasedViewResolver faceletsViewResolver() {
 
-    @Bean
-    public SimpleControllerHandlerAdapter simpleControllerHandlerAdapter() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setViewClass(JsfView.class);
+		resolver.setPrefix("/WEB-INF/");
+		resolver.setSuffix(".xhtml");
+		return resolver;
+	}
 
-        return new SimpleControllerHandlerAdapter();
-    }
+
+	@Bean
+	public SimpleControllerHandlerAdapter simpleControllerHandlerAdapter() {
+
+		return new SimpleControllerHandlerAdapter();
+	}
 
 
+	/**
+	 * Register resource handler for images, scrips and css under /webapp in Spring-Boot start configuration.
+	 *
+	 * @param registry
+	 * @since 05.2020
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-    /**
-     * Register resource handler for images, scrips and css under /webapp in Spring-Boot start configuration.
-     *
-     * @param registry
-     * @since 05.2020
-     */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-        registry.addResourceHandler("/images/**").addResourceLocations("/images/").setCacheControl(CACHE_PUBLIC);
-        registry.addResourceHandler("/styles/**").addResourceLocations("/styles/").setCacheControl(CACHE_PUBLIC);
-        registry.addResourceHandler("/WEB-INF/**").addResourceLocations("/spring/").setCacheControl(CACHE_PUBLIC);
-        log.debug(format("registry= %s", registry));
-    }
+		registry
+				.addResourceHandler("/images/**")
+				.addResourceLocations("/images/")
+				.setCacheControl(CACHE_PUBLIC);
+		registry
+				.addResourceHandler("/styles/**")
+				.addResourceLocations("/styles/")
+				.setCacheControl(CACHE_PUBLIC);
+		//registry.addResourceHandler("/WEB-INF/**").addResourceLocations("/spring/").setCacheControl(CACHE_PUBLIC);
+		log.debug(format("registry= %s", registry));
+	}
 
 }
