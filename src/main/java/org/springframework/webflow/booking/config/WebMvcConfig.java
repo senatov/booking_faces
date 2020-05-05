9044b6e97238da2b7de50237336f958f34f356a9
Iakov Senatov <javaEntwickler@gmail.com>
2020-05-05 05:00:35 +0200

    OK/Tested: styles and images under /resources/**

--- src/main/java/org/springframework/webflow/booking/config/WebMvcConfig.java
+++ src/main/java/org/springframework/webflow/booking/config/WebMvcConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.faces.mvc.JsfView;
 import org.springframework.faces.webflow.JsfFlowHandlerAdapter;
 import org.springframework.http.CacheControl;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.SimpleControllerHandlerAdapter;
 import org.springframework.web.servlet.mvc.UrlFilenameViewController;
@@ -17,7 +16,6 @@ import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import org.springframework.webflow.mvc.servlet.FlowHandlerAdapter;
 import org.springframework.webflow.mvc.servlet.FlowHandlerMapping;
 
-import static java.lang.String.format;
 import static java.util.concurrent.TimeUnit.HOURS;
 
 
@@ -26,70 +24,72 @@ import static java.util.concurrent.TimeUnit.HOURS;
 @Slf4j
 public class WebMvcConfig implements WebMvcConfigurer {
 
-	public static final CacheControl CACHE_PUBLIC = CacheControl
-			.maxAge(2, HOURS)
-			.cachePublic();
-	@Autowired private WebFlowConfig webFlowConfig;
+    public static final CacheControl CACHE_PUBLIC = CacheControl.maxAge(2, HOURS).cachePublic();
+    @Autowired
+    private WebFlowConfig webFlowConfig;
 
 
-	@Bean
-	public FlowHandlerMapping flowHandlerMapping() {
 
-		FlowHandlerMapping mapping = new FlowHandlerMapping();
-		mapping.setOrder(1);
-		mapping.setFlowRegistry(webFlowConfig.flowRegistry());
-		/* If no flow matches, map the path to a view, e.g. "/intro" maps to a view named "intro" */
-		mapping.setDefaultHandler(new UrlFilenameViewController());
-		return mapping;
-	}
+    @Bean
+    public FlowHandlerMapping flowHandlerMapping() {
 
-
-	@Bean
-	public FlowHandlerAdapter flowHandlerAdapter() {
-
-		JsfFlowHandlerAdapter adapter = new JsfFlowHandlerAdapter();
-		adapter.setFlowExecutor(webFlowConfig.flowExecutor());
-		return adapter;
-	}
+        FlowHandlerMapping mapping = new FlowHandlerMapping();
+        mapping.setOrder(1);
+        mapping.setFlowRegistry(webFlowConfig.flowRegistry());
+        /* If no flow matches, map the path to a view, e.g. "/intro" maps to a view named "intro" */
+        mapping.setDefaultHandler(new UrlFilenameViewController());
+        return mapping;
+    }
 
 
-	@Bean
-	public UrlBasedViewResolver faceletsViewResolver() {
 
-		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
-		resolver.setViewClass(JsfView.class);
-		resolver.setPrefix("/WEB-INF/");
-		resolver.setSuffix(".xhtml");
-		return resolver;
-	}
+    @Bean
+    public FlowHandlerAdapter flowHandlerAdapter() {
 
-
-	@Bean
-	public SimpleControllerHandlerAdapter simpleControllerHandlerAdapter() {
-
-		return new SimpleControllerHandlerAdapter();
-	}
+        JsfFlowHandlerAdapter adapter = new JsfFlowHandlerAdapter();
+        adapter.setFlowExecutor(webFlowConfig.flowExecutor());
+        return adapter;
+    }
 
 
-	/**
-	 * Register resource handler for images, scrips and css under /webapp in Spring-Boot start configuration.
-	 *
-	 * @param registry
-	 * @since 05.2020
-	 */
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-		registry
-				.addResourceHandler("/images/**")
-				.addResourceLocations("/images/")
-				.setCacheControl(CACHE_PUBLIC);
-		registry
-				.addResourceHandler("/styles/**")
-				.addResourceLocations("/styles/")
-				.setCacheControl(CACHE_PUBLIC);
-		//registry.addResourceHandler("/WEB-INF/**").addResourceLocations("/spring/").setCacheControl(CACHE_PUBLIC);
-		log.debug(format("registry= %s", registry));
-	}
+    @Bean
+    public UrlBasedViewResolver faceletsViewResolver() {
 
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        resolver.setViewClass(JsfView.class);
+        resolver.setPrefix("/WEB-INF/");
+        resolver.setSuffix(".xhtml");
+        return resolver;
+    }
+
+
+
+    @Bean
+    public SimpleControllerHandlerAdapter simpleControllerHandlerAdapter() {
+
+        return new SimpleControllerHandlerAdapter();
+    }
+
+    /**
+     * Register resource handler for images, scrips and css under /webapp in Spring-Boot start configuration.
+     *
+     * @param registry
+     * @since 05.2020
+     */
+    /**
+     @Override public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+     registry
+     .addResourceHandler("/images/**")
+     .addResourceLocations("/images/")
+     .setCacheControl(CACHE_PUBLIC);
+     registry
+     .addResourceHandler("/styles/**")
+     .addResourceLocations("/styles/")
+     .setCacheControl(CACHE_PUBLIC);
+     //registry.addResourceHandler("/WEB-INF/**").addResourceLocations("/spring/").setCacheControl(CACHE_PUBLIC);
+     log.debug(format("registry= %s", registry));
+     }
+     **/
 }
