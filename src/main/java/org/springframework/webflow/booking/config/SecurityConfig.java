b787d00fa9c86d1e838e58930a62f334beeb5b41
Iakov Senatov <javaEntwickler@gmail.com>
2020-05-08 09:49:01 +0200

    OK/Tested: add in security configure()

--- src/main/java/org/springframework/webflow/booking/config/SecurityConfig.java
+++ src/main/java/org/springframework/webflow/booking/config/SecurityConfig.java
@@ -15,53 +15,45 @@ import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
 
-        http
-
-                /**
-                 .authorizeRequests()
-                 .antMatchers("/resources/images/**", "/resources/styles/**", "/intro/**", "/login/**")
-                 .permitAll()
-                 .anyRequest()
-                 .authenticated()
-                 .and()
-                 */.formLogin()
-                .loginPage("/spring/login")
-                .loginProcessingUrl("/spring/loginProcess")
-                .defaultSuccessUrl("/spring/main")
-                .failureUrl("/spring/login?login_error=1")
-                .and()
-                .logout()
-                .logoutUrl("/spring/logout")
-                .logoutSuccessUrl("/spring/logoutSuccess")
-                .and()
-                .requestCache()
-                .requestCache(new HttpSessionRequestCache());
-    }
+		http
+				.formLogin()
+				.loginPage("/spring/login")
+				.loginProcessingUrl("/spring/loginProcess")
+				.defaultSuccessUrl("/spring/main")
+				.failureUrl("/spring/login?login_error=1")
+				.and()
+				.logout()
+				.logoutUrl("/spring/logout")
+				.logoutSuccessUrl("/spring/logoutSuccess")
+				.and()
+				.requestCache()
+				.requestCache(new HttpSessionRequestCache());
+	}
 
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.inMemoryAuthentication()
-                .withUser("keith")
-                .password("{MD5}417c7382b16c395bc25b5da1398cf076")
-                .roles("USER", "SUPERVISOR")
-                .and()
-                .withUser("erwin")
-                .password("{MD5}12430911a8af075c6f41c6976af22b09")
-                .roles("USER", "SUPERVISOR")
-                .and()
-                .withUser("jeremy")
-                .password("{MD5}57c6cbff0d421449be820763f03139eb")
-                .roles("USER")
-                .and()
-                .withUser("scott")
-                .password("{MD5}942f2339bf50796de535a384f0d1af3e")
-                .roles("USER");
-    }
+		auth
+				.inMemoryAuthentication()
+				.withUser("keith")
+				.password("{MD5}417c7382b16c395bc25b5da1398cf076")
+				.roles("USER", "SUPERVISOR")
+				.and()
+				.withUser("erwin")
+				.password("{MD5}12430911a8af075c6f41c6976af22b09")
+				.roles("USER", "SUPERVISOR")
+				.and()
+				.withUser("jeremy")
+				.password("{MD5}57c6cbff0d421449be820763f03139eb")
+				.roles("USER")
+				.and()
+				.withUser("scott")
+				.password("{MD5}942f2339bf50796de535a384f0d1af3e")
+				.roles("USER");
+	}
 
 }
