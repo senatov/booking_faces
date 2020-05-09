923007209a726b0e7f01fae40460ab167ee7841a
Iakov Senatov <javaEntwickler@gmail.com>
2020-05-09 19:55:57 +0200

    OK/Tested: formatted

--- src/main/java/org/springframework/webflow/booking/config/WebMvcConfig.java
+++ src/main/java/org/springframework/webflow/booking/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package org.springframework.webflow.booking.config;
 
 
+
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,67 +22,67 @@ import org.springframework.webflow.mvc.servlet.FlowHandlerMapping;
 import static java.util.concurrent.TimeUnit.HOURS;
 
 
+
 @EnableWebMvc
 @Configuration
 @Slf4j
 @ToString
 public class WebMvcConfig implements WebMvcConfigurer {
 
-  public static final CacheControl CACHE_PUBLIC = CacheControl.maxAge(2, HOURS).cachePublic();
-  @Autowired
-  private WebFlowConfig webFlowConfig;
+    public static final CacheControl CACHE_PUBLIC = CacheControl.maxAge(2, HOURS).cachePublic();
+    @Autowired
+    private WebFlowConfig webFlowConfig;
 
 
-  @Bean
-  public FlowHandlerMapping flowHandlerMapping() {
 
-    FlowHandlerMapping mapping = new FlowHandlerMapping();
-    mapping.setOrder(1);
-    mapping.setFlowRegistry(webFlowConfig.flowRegistry());
-    /* If no flow matches, map the path to a view, e.g. "/intro" maps to a view named "intro" */
-    mapping.setDefaultHandler(new UrlFilenameViewController());
-    return mapping;
-  }
+    @Bean
+    public FlowHandlerMapping flowHandlerMapping() {
 
-
-  @Bean
-  public FlowHandlerAdapter flowHandlerAdapter() {
-
-    JsfFlowHandlerAdapter adapter = new JsfFlowHandlerAdapter();
-    adapter.setFlowExecutor(webFlowConfig.flowExecutor());
-    return adapter;
-  }
+        FlowHandlerMapping mapping = new FlowHandlerMapping();
+        mapping.setOrder(1);
+        mapping.setFlowRegistry(webFlowConfig.flowRegistry());
+        /* If no flow matches, map the path to a view, e.g. "/intro" maps to a view named "intro" */
+        mapping.setDefaultHandler(new UrlFilenameViewController());
+        return mapping;
+    }
 
 
-  @Bean
-  public UrlBasedViewResolver faceletsViewResolver() {
 
-    UrlBasedViewResolver resolver = new UrlBasedViewResolver();
-    resolver.setViewClass(JsfView.class);
-    resolver.setPrefix("/WEB-INF/");
-    resolver.setSuffix(".xhtml");
-    return resolver;
-  }
+    @Bean
+    public FlowHandlerAdapter flowHandlerAdapter() {
 
-
-  @Bean
-  public SimpleControllerHandlerAdapter simpleControllerHandlerAdapter() {
-
-    return new SimpleControllerHandlerAdapter();
-  }
+        JsfFlowHandlerAdapter adapter = new JsfFlowHandlerAdapter();
+        adapter.setFlowExecutor(webFlowConfig.flowExecutor());
+        return adapter;
+    }
 
 
-  /**
-   * Register resource handler for images, scrips and css under /webapp in Spring-Boot start configuration.
-   *
-   * @param registry
-   * @since 05.2020
-   */
-  @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-    registry.addResourceHandler("/spring/images/**").addResourceLocations("/images/").setCacheControl(CACHE_PUBLIC);
-    registry.addResourceHandler("/spring/styles/**").addResourceLocations("/styles/").setCacheControl(CACHE_PUBLIC);
-  }
+    @Bean
+    public UrlBasedViewResolver faceletsViewResolver() {
+
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        resolver.setViewClass(JsfView.class);
+        resolver.setPrefix("/WEB-INF/");
+        resolver.setSuffix(".xhtml");
+        return resolver;
+    }
+
+
+
+    @Bean
+    public SimpleControllerHandlerAdapter simpleControllerHandlerAdapter() {
+
+        return new SimpleControllerHandlerAdapter();
+    }
+
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/styles/**").addResourceLocations("/styles/");
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+    }
 
 }
